50d8e85413a61ccb6a17e3fa932d97e7d6bd414f
loading1031 <tjdans1031@gmail.com>
2024-08-29 22:59:33 +0900

    fix: 오타 수정

---
+++ golbang_jb/lib/pages/group/widgets/admin_button_widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class AdminAddButton extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  // onPressed를 외부에서 주입받기 위한 생성자
+  AdminAddButton({required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton.icon(
+        onPressed: onPressed,
+        icon: Icon(
+          Icons.add,
+          color: Colors.green,
+        ),
+        label: Text(
+          '관리자',
+          style: TextStyle(
+            color: Colors.green,
+          ),
+        ),
+        style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.green,
+          backgroundColor: Colors.white, // 버튼 텍스트 및 아이콘 색상
+          side: BorderSide(color: Colors.green), // 테두리 색상
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0), // 둥근 모서리
+          ),
+        ),
+      ),
+    );
+  }
+}
