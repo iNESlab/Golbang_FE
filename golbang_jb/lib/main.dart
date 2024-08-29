372b20956193ebad172edab6f626c3f0405b7b34
loading1031 <tjdans1031@gmail.com>
2024-08-29 23:01:52 +0900

    feat&bug: 모임 생성 페이지 api 연동(post 미동작)

--- golbang_jb/lib/main.dart
+++ golbang_jb/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:golbang/pages/logins/hi_screen.dart';
 import 'package:golbang/pages/signup/signup.dart';
 import 'package:golbang/pages/signup/signup_complete.dart';
+<<<<<<< HEAD
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:provider/provider.dart';
 import 'package:golbang/provider/user_token_provider.dart';
@@ -13,6 +15,24 @@ import 'package:golbang/pages/home/home_page.dart';
 Future<void> main() async {
   await dotenv.load(fileName: 'assets/config/.env');
   initializeDateFormatting().then((_) => runApp(const MyApp()));
+=======
+import 'package:golbang/pages/home/home_page.dart';
+import 'package:golbang/pages/game/score_card_page.dart';
+import 'package:golbang/provider/user_token_provider.dart';
+import 'package:golbang/repoisitory/secure_storage.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+Future<void> main() async {
+  await dotenv.load(fileName: 'assets/config/.env');
+  // 날짜 형식화를 초기화한 후 앱을 시작합니다.
+  initializeDateFormatting().then((_) {
+    runApp(
+      ProviderScope(
+        child: const MyApp(),
+      ),
+    );
+  });
+>>>>>>> 5f854be (feat&bug: 모임 생성 페이지 api 연동(post 미동작))
 }
 
 class MyApp extends StatelessWidget {
@@ -20,30 +40,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ProviderScope(
-        child: MaterialApp(
-          title: 'GOLBANG MAIN PAGE',
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            primarySwatch: Colors.green,
-            appBarTheme: const AppBarTheme(
-              color: Colors.white,
-              foregroundColor: Colors.black,
-            ),
-            bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-              backgroundColor: Colors.white,
-              selectedItemColor: Colors.black,
-              unselectedItemColor: Colors.grey,
-            ),
-            scaffoldBackgroundColor: Colors.white,
-            useMaterial3: true,
-          ),
-          home: const HiScreen(),
-          routes: {
-            '/signup': (context) => SignUpPage(),
-            '/signupComplete': (context) => const SignupComplete(),
-          },
-        )
+    return MaterialApp(
+      title: 'GOLBANG MAIN PAGE',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        appBarTheme: const AppBarTheme(
+          color: Colors.white,
+          foregroundColor: Colors.black,
+        ),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          backgroundColor: Colors.white,
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.grey,
+        ),
+        scaffoldBackgroundColor: Colors.white,
+        useMaterial3: true,
+      ),
+      home: const HiScreen(),
+      routes: {
+        '/signup': (context) => SignUpPage(),
+        '/signupComplete': (context) => const SignupComplete(),
+        // 추가적인 라우트를 여기에 추가할 수 있습니다.
+      },
     );
   }
 }
--- golbang_jb/lib/services/user_service.dart
+++ golbang_jb/lib/services/user_service.dart
@@ -2,8 +2,88 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
+import 'package:jwt_decoder/jwt_decoder.dart';
+import '../models/user_profile.dart';
+import '../repoisitory/secure_storage.dart';
 
 class UserService {
+  final SecureStorage storage;
+
+  UserService(this.storage);
+
+  Future<List<UserProfile>> getUserProfileList() async {
+    // 액세스 토큰 불러오기
+    final accessToken = await storage.readAccessToken();
+
+    // API URI 설정
+    var uri = Uri.parse("${dotenv.env['API_HOST']}/api/v1/users/info/");
+
+    // 요청 헤더 설정
+    Map<String, String> headers = {
+      "Content-type": "application/json",
+      "Authorization": "Bearer $accessToken"
+    };
+
+    // API 요청
+    var response = await http.get(uri, headers: headers);
+
+    print("${json.decode(utf8.decode(response.bodyBytes))}");
+
+    // 응답 코드가 200(성공)인지 확인
+    if (response.statusCode == 200) {
+      // JSON 데이터 파싱
+      var jsonData = json.decode(utf8.decode(response.bodyBytes));
+      print("json: ${jsonData['data']}");
+
+      return (jsonData['data'] as List)
+          .map((json) => UserProfile.fromJson(json))
+          .toList();
+
+    } else {
+      // 오류 발생 시 예외를 던짐
+      throw Exception('Failed to load user profiles');
+    }
+  }
+
+  Future<UserProfile> getUserProfile() async {
+    try {
+      // 액세스 토큰 불러오기
+      final accessToken = await storage.readAccessToken();
+      Map<String, dynamic> decodedToken = JwtDecoder.decode(accessToken);
+      print('decodedToken: $decodedToken');
+      String userId = decodedToken['user_id'].toString(); // payload에서 user_id 추출
+      print('userId from token $userId');
+      var uri = Uri.parse("${dotenv.env['API_HOST']}/api/v1/users/info/$userId/");
+
+      // 요청 헤더 설정
+      Map<String, String> headers = {
+        "Content-type": "application/json",
+        "Authorization": "Bearer $accessToken"
+      };
+
+      // API 요청
+      var response = await http.get(uri, headers: headers);
+
+      print("${json.decode(utf8.decode(response.bodyBytes))}");
+
+      // 응답 코드가 200(성공)인지 확인
+      if (response.statusCode == 200) {
+        // JSON 데이터 파싱
+        var jsonData = json.decode(utf8.decode(response.bodyBytes));
+        print("json: ${jsonData}");
+        // 이유는 모르나 code / message /data 형태로 반환이 안됨. data만 반환됨
+        return UserProfile.fromJson(jsonData);
+      } else {
+        throw Exception('Failed to load user profiles');
+      }
+    } catch (e) {
+      print('Error decoding token: $e');
+      // 예외를 다시 던져서 함수가 항상 반환값을 가지도록 함
+      throw Exception('Error decoding token: $e');
+    }
+  }
+
+
   static Future<http.Response> saveUser({
     required String userId,
     required String email,
