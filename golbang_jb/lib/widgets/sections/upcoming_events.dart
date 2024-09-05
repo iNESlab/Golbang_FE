da07290cc9b72e0f049f75f8013c01f9be344d63
Mingguriguri <rlaalswjd-@naver.com>
2024-09-05 18:01:59 +0900

    feat(event): event_main에서 이벤트 상세 조회가 되듯이, home_main에서도 이벤트 항목을 클릭하면 이벤트 상세 조회 페이지로 넘어가도록 설정

--- golbang_jb/lib/widgets/sections/upcoming_events.dart
+++ golbang_jb/lib/widgets/sections/upcoming_events.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:golbang/models/event.dart';
+import 'package:golbang/pages/event/event_detail.dart'; // EventDetailPage를 불러오기 위한 import 추가
 
 class UpcomingEvents extends StatelessWidget {
   final List<Event> events;
@@ -21,58 +22,62 @@ class UpcomingEvents extends StatelessWidget {
             // 첫 번째 참여자의 상태를 사용하여 테두리 색상 설정
             String? statusType = event.participants.isNotEmpty ? event.participants[0].statusType : '미정';
 
-            return Container(
-              margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
-              padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(
-                  color: _getBorderColor(statusType!),
-                  width: 1.5,
-                ),
-                borderRadius: BorderRadius.circular(15.0),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 2,
-                    blurRadius: 5,
-                    offset: const Offset(0, 3),
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => EventDetailPage(event: event),
                   ),
-                ],
-              ),
-              child: ListTile(
-                contentPadding: const EdgeInsets.all(4),
-                title: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        Text(
-                          event.eventTitle,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        // if (event.isCompleted)
-                        //   const Icon(
-                        //     Icons.admin_panel_settings,
-                        //     color: Colors.green,
-                        //     size: 25,
-                        //   ),
-                      ],
-                    ),
-                    Text(
-                      '${event.startDateTime.toLocal()}',
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    Text('장소: ${event.location}'),
-                    Row(
-                      children: [
-                        const Text('참석 여부: '),
-                        _buildStatusButton(statusType),
-                      ],
+                );
+              },
+              child: Container(
+                margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+                padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(
+                    color: _getBorderColor(statusType!),
+                    width: 1.5,
+                  ),
+                  borderRadius: BorderRadius.circular(15.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: const Offset(0, 3),
                     ),
                   ],
+                ),
+                child: ListTile(
+                  contentPadding: const EdgeInsets.all(4),
+                  title: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            event.eventTitle,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        '${event.startDateTime.toLocal()}',
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text('장소: ${event.location}'),
+                      Row(
+                        children: [
+                          const Text('참석 여부: '),
+                          _buildStatusButton(statusType),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             );
