ba3fd94e9b11bc0eb3e26c0b5ac467c09d0a6795
Mingguriguri <rlaalswjd-@naver.com>
2024-09-13 17:17:25 +0900

    fix(event): 이벤트 수정시, 조에 맞게 참여자들이 나온 후 다시 조를 수정하는 기능이 제대로 동작하지 않아 다시 원래대로(=조가 모두 초기화)되도록 수정

--- golbang_jb/lib/pages/event/event_update2.dart
+++ golbang_jb/lib/pages/event/event_update2.dart
@@ -5,11 +5,10 @@ import 'package:golbang/pages/event/widgets/no_api_participant_dialog.dart';
 import 'package:golbang/pages/event/widgets/toggle_bottons.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import '../../models/club.dart';
+import '../../models/create_event.dart';
 import '../../models/create_participant.dart';
 import '../../models/enum/event.dart';
 import '../../models/member_profile.dart';
-import '../../models/participant.dart';
-import '../../models/update_event_participant.dart';
 import '../../repoisitory/secure_storage.dart';
 import '../../services/event_service.dart';
 
@@ -20,7 +19,7 @@ class EventsUpdate2 extends ConsumerStatefulWidget {
   final LatLng? selectedLocation;
   final DateTime startDate;
   final DateTime endDate;
-  final List<UpdateEventParticipant> selectedParticipants;
+  final List<ClubMemberProfile> selectedParticipants;
   final GameMode selectedGameMode;
 
   EventsUpdate2({
@@ -41,14 +40,15 @@ class EventsUpdate2 extends ConsumerStatefulWidget {
 class _EventsUpdate2State extends ConsumerState<EventsUpdate2> {
   GameMode gameMode = GameMode.STROKE;
   TeamConfig teamConfig = TeamConfig.NONE;
-  String groupSetting = 'NONE'; // 기본값을 TeamConfig의 기본값과 일치시킴
-  String numberOfGroups = '8';  // 예시로 '8개' 대신 '8'로 초기화
-  String numberOfPlayers = '4'; // '4명' 대신 '4'로 초기화
+  String groupSetting = '직접 설정';
+  String numberOfGroups = '8개';
+  String numberOfPlayers = '4명';
   bool isAutoMatching = false;
   bool isTeam = false;
   List<Map<String, List<CreateParticipant>>> groups = [];
   List<CreateParticipant> _selectedParticipants = [];
   bool hasDuplicateParticipants = false;
+  bool areGroupsEmpty = true;
   bool allParticipantsAssigned = false;
   late EventService _eventService;
 
@@ -56,61 +56,25 @@ class _EventsUpdate2State extends ConsumerState<EventsUpdate2> {
   void initState() {
     super.initState();
     _eventService = EventService(ref.read(secureStorageProvider));
-    gameMode = widget.selectedGameMode; // 기존에 설정된 게임 모드 초기화
     _initializeParticipants();
-  }
-
-  // String을 TeamConfig로 변환하는 메서드
-  TeamConfig _getTeamConfigFromString(String groupSetting) {
-    switch (groupSetting) {
-      case 'NONE':
-        return TeamConfig.NONE;
-      case 'A':
-        return TeamConfig.TEAM_A;
-      case 'B':
-        return TeamConfig.TEAM_B;
-      default:
-        return TeamConfig.NONE; // 기본값
-    }
-  }
-
-  String _getStringFromTeamConfig(TeamConfig config) {
-    return config.value; // TeamConfig를 String으로 변환
-  }
-
-  int _parseStringToInt(String value) {
-    return int.parse(value.replaceAll(RegExp(r'[^0-9]'), '')); // '개', '명' 같은 문자를 제거하고 숫자로 변환
+    gameMode = widget.selectedGameMode; // 기존에 설정된 게임 모드 초기화
   }
 
   void _initializeParticipants() {
-    Map<int, List<CreateParticipant>> participantsByGroup = {};
-
-    for (var participant in widget.selectedParticipants) {
-      int groupType = participant.groupType;
-
-      participantsByGroup.putIfAbsent(groupType, () => []);
-
-      participantsByGroup[groupType]!.add(CreateParticipant(
+    // 기존 참가자 정보를 CreateParticipant로 변환하여 초기화
+    _selectedParticipants = widget.selectedParticipants.map((participant) {
+      return CreateParticipant(
         memberId: participant.memberId,
         name: participant.name,
         profileImage: participant.profileImage,
-        teamType: participant.teamType,
-        groupType: participant.groupType,
-      ));
-    }
-
-    setState(() {
-      groups = participantsByGroup.entries.map((entry) {
-        return {
-          '조${entry.key}': entry.value,
-        };
-      }).toList();
-      _validateForm();
-    });
+        teamType: teamConfig,
+        groupType: groupSetting,
+      );
+    }).toList();
   }
 
   void _createGroups() {
-    int numGroups = _parseStringToInt(numberOfGroups); // String을 int로 변환
+    int numGroups = int.parse(numberOfGroups.replaceAll('개', ''));
     setState(() {
       groups = List.generate(
         numGroups,
@@ -127,10 +91,12 @@ class _EventsUpdate2State extends ConsumerState<EventsUpdate2> {
     for (var group in groups) {
       for (var participant in group.values.first) {
         if (!allParticipants.add(participant.memberId)) {
+          print('참가자 중복입니다.true');
           return true;
         }
       }
     }
+    print('참가자 중복이 아닙니다.false');
     return false;
   }
 
@@ -141,6 +107,7 @@ class _EventsUpdate2State extends ConsumerState<EventsUpdate2> {
         assignedParticipants.add(participant.memberId);
       }
     }
+    print('참가자 할당여부 ${assignedParticipants.length == _selectedParticipants.length}');
     return assignedParticipants.length == _selectedParticipants.length;
   }
 
@@ -148,6 +115,7 @@ class _EventsUpdate2State extends ConsumerState<EventsUpdate2> {
     setState(() {
       hasDuplicateParticipants = _checkForDuplicateParticipants();
       allParticipantsAssigned = _checkIfAllParticipantsAssigned();
+      print("---------------------------------------------------");
     });
   }
 
@@ -164,7 +132,8 @@ class _EventsUpdate2State extends ConsumerState<EventsUpdate2> {
           selectedParticipants: groupParticipants,
           onSelectionComplete: (List<CreateParticipant> updatedParticipants) {
             setState(() {
-              groups.firstWhere((group) => group.keys.first == groupName)[groupName] =
+              groups
+                  .firstWhere((group) => group.keys.first == groupName)[groupName] =
                   updatedParticipants;
               _validateForm();
             });
@@ -188,7 +157,9 @@ class _EventsUpdate2State extends ConsumerState<EventsUpdate2> {
 
     if (success) {
       Navigator.of(context).pop(true);
+      Navigator.of(context).pop(true);
       ScaffoldMessenger.of(context).showSnackBar(
+
         SnackBar(content: Text('이벤트가 성공적으로 수정되었습니다.')),
       );
     } else {
@@ -228,155 +199,149 @@ class _EventsUpdate2State extends ConsumerState<EventsUpdate2> {
         elevation: 0,
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              DropdownButtonFormField<GameMode>(
-                decoration: InputDecoration(
-                  labelText: '게임모드',
-                  border: OutlineInputBorder(),
-                ),
-                value: gameMode,
-                onChanged: (newValue) {
-                  setState(() {
-                    gameMode = newValue!;
-                    _validateForm();
-                  });
-                },
-                items: GameMode.values.map((mode) {
-                  return DropdownMenuItem<GameMode>(
-                    value: mode,
-                    child: Text(
-                      mode == GameMode.STROKE ? '스트로크' : mode.toString(),
-                    ),
-                  );
-                }).toList(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            DropdownButtonFormField<GameMode>(
+              decoration: InputDecoration(
+                labelText: '게임모드',
+                border: OutlineInputBorder(),
               ),
-              SizedBox(height: 20),
-              ToggleButtonsWidget(
-                onSelectedMatchingType: (int index) {
-                  setState(() {
-                    isAutoMatching = index == 0;
-                  });
-                },
-                onSelectedTeamType: (int index) {
-                  setState(() {
-                    isTeam = index == 1;
-                  });
-                },
-              ),
-              SizedBox(height: 20),
-              // groupSetting을 String으로 사용하되, TeamConfig로 변환할 때만 사용
-              DropdownButtonFormField<TeamConfig>(
-                decoration: InputDecoration(
-                  labelText: '팀 설정',
-                  border: OutlineInputBorder(),
-                ),
-                value: _getTeamConfigFromString(groupSetting), // String을 TeamConfig로 변환
-                onChanged: (newValue) {
-                  setState(() {
-                    teamConfig = newValue!;
-                    groupSetting = _getStringFromTeamConfig(newValue); // TeamConfig를 String으로 변환
-                  });
-                },
-                items: TeamConfig.values.map((config) {
-                  return DropdownMenuItem<TeamConfig>(
-                    value: config,
-                    child: Text(config.value),
-                  );
-                }).toList(),
-              ),
-              SizedBox(height: 20),
-              Row(
-                children: [
-                  Expanded(
-                    child: DropdownButtonFormField<String>(
-                      decoration: InputDecoration(
-                        labelText: '팀 수',
-                        border: OutlineInputBorder(),
-                      ),
-                      value: numberOfGroups,
-                      onChanged: (newValue) {
-                        setState(() {
-                          numberOfGroups = newValue!;
-                        });
-                      },
-                      items: ['8', '7', '6', '5', '4', '3', '2', '1'].map((number) {
-                        return DropdownMenuItem<String>(
-                          value: number,
-                          child: Text('$number개'), // Display with '개' but store as number
-                        );
-                      }).toList(),
-                    ),
+              value: gameMode,
+              onChanged: (newValue) {
+                setState(() {
+                  gameMode = newValue!;
+                  _validateForm();
+                });
+              },
+              items: GameMode.values.map((mode) {
+                return DropdownMenuItem<GameMode>(
+                  value: mode,
+                  child: Text(
+                    mode == GameMode.STROKE ? '스트로크' : mode.toString(),
                   ),
-                  SizedBox(width: 20),
-                  Expanded(
-                    child: DropdownButtonFormField<String>(
-                      decoration: InputDecoration(
-                        labelText: '인원 수',
-                        border: OutlineInputBorder(),
-                      ),
-                      value: numberOfPlayers,
-                      onChanged: (newValue) {
-                        setState(() {
-                          numberOfPlayers = newValue!;
-                        });
-                      },
-                      items: ['4', '3', '2'].map((number) {
-                        return DropdownMenuItem<String>(
-                          value: number,
-                          child: Text('$number명'), // Display with '명' but store as number
-                        );
-                      }).toList(),
+                );
+              }).toList(),
+            ),
+            SizedBox(height: 20),
+            ToggleButtonsWidget(
+              onSelectedMatchingType: (int index) {
+                setState(() {
+                  isAutoMatching = index == 0;
+                });
+              },
+              onSelectedTeamType: (int index) {
+                setState(() {
+                  isTeam = index == 1;
+                });
+              },
+            ),
+            SizedBox(height: 20),
+            DropdownButtonFormField<String>(
+              decoration: InputDecoration(
+                labelText: '직접 설정',
+                border: OutlineInputBorder(),
+              ),
+              value: groupSetting,
+              onChanged: (newValue) {
+                setState(() {
+                  groupSetting = newValue!;
+                });
+              },
+              items: ['직접 설정'].map((setting) {
+                return DropdownMenuItem<String>(
+                  value: setting,
+                  child: Text(setting),
+                );
+              }).toList(),
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                Expanded(
+                  child: DropdownButtonFormField<String>(
+                    decoration: InputDecoration(
+                      labelText: '팀 수',
+                      border: OutlineInputBorder(),
                     ),
+                    value: numberOfGroups,
+                    onChanged: (newValue) {
+                      setState(() {
+                        numberOfGroups = newValue!;
+                      });
+                    },
+                    items: ['8개', '7개', '6개', '5개', '4개', '3개', '2개', '1개'].map((number) {
+                      return DropdownMenuItem<String>(
+                        value: number,
+                        child: Text(number),
+                      );
+                    }).toList(),
                   ),
-                ],
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _createGroups,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.teal,
-                  minimumSize: Size(double.infinity, 50),
                 ),
-                child: Text('조 생성'),
+                SizedBox(width: 20),
+                Expanded(
+                  child: DropdownButtonFormField<String>(
+                    decoration: InputDecoration(
+                      labelText: '인원 수',
+                      border: OutlineInputBorder(),
+                    ),
+                    value: numberOfPlayers,
+                    onChanged: (newValue) {
+                      setState(() {
+                        numberOfPlayers = newValue!;
+                      });
+                    },
+                    items: ['4명', '3명', '2명'].map((number) {
+                      return DropdownMenuItem<String>(
+                        value: number,
+                        child: Text(number),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _createGroups,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.teal,
+                minimumSize: Size(double.infinity, 50),
               ),
-              if (groups.isNotEmpty)
-                Padding(
-                  padding: const EdgeInsets.only(top: 20.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('추가 버튼을 눌러 멤버를 추가해보세요'),
-                      SizedBox(height: 10),
-                      SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: groups.map((group) {
-                            String groupName = group.keys.first;
-                            List<CreateParticipant> members = group[groupName]!;
+              child: Text('조 생성'),
+            ),
+            if (groups.isNotEmpty)
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('추가 버튼을 눌러 멤버를 추가해보세요'),
+                    SizedBox(height: 10),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start, // 상단 정렬 설정
+                        children: groups.map((group) {
+                          String groupName = group.keys.first;
+                          List<CreateParticipant> members = group[groupName]!;
 
-                            return GroupCard(
-                              groupName: groupName,
-                              members: members,
-                              onAddParticipant: () {
-                                _showParticipantSelectionDialog(groupName);
-                              },
-                              buttonTextStyle: TextStyle(color: Colors.white),
-                            );
-                          }).toList(),
-                        ),
+                          return GroupCard(
+                            groupName: groupName,
+                            members: members,
+                            onAddParticipant: () {
+                              _showParticipantSelectionDialog(groupName);
+                            }, buttonTextStyle: TextStyle(color: Colors.white),
+                          );
+                        }).toList(),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-            ],
-          ),
+              ),
+          ],
         ),
       ),
     );
