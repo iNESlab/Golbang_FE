dec6ab9b23cad5147fbf57711ba8859213fe00a4
Minjeong Kim <101111603+Mingguriguri@users.noreply.github.com>
2024-09-18 11:57:31 +0900

    design(account): 핸디캡 필드를 number picker로 구성하고, CSS를 메인 컬러에 맞게 수정

--- golbang_jb/lib/pages/profile/user_info_page.dart
+++ golbang_jb/lib/pages/profile/user_info_page.dart
@@ -5,6 +5,7 @@ import 'package:image_picker/image_picker.dart';
 import '../../models/user_account.dart';
 import '../../services/user_service.dart';
 import '../../services/user_service_provider.dart';
+import 'package:numberpicker/numberpicker.dart';
 
 class UserInfoPage extends ConsumerStatefulWidget {
   final UserAccount initialUserAccount;
@@ -14,6 +15,7 @@ class UserInfoPage extends ConsumerStatefulWidget {
   @override
   _UserInfoPageState createState() => _UserInfoPageState();
 }
+
 
 class _UserInfoPageState extends ConsumerState<UserInfoPage> {
   final ImagePicker _picker = ImagePicker();
@@ -45,7 +47,6 @@ class _UserInfoPageState extends ConsumerState<UserInfoPage> {
         phoneNumber: field == '연락처' ? value : _userAccount.phoneNumber,
         handicap: field == '핸디캡' ? int.tryParse(value ?? _userAccount.handicap.toString()) : _userAccount.handicap,
         address: field == '집 주소' ? value : _userAccount.address,
-        // 생일은 DateTime 형식으로 변환 후 전달
         dateOfBirth: field == '생일' ? DateTime.parse(value!) : _userAccount.dateOfBirth,
         studentId: field == '학번' ? value : _userAccount.studentId,
         profileImage: _imageFile != null ? File(_imageFile!.path) : null,
@@ -116,7 +117,7 @@ class _UserInfoPageState extends ConsumerState<UserInfoPage> {
           _buildEditableListTile('이름', _userAccount.name, true),
           _buildEditableListTile('이메일', _userAccount.email, true),
           _buildEditableListTile('연락처', _userAccount.phoneNumber, true),
-          _buildEditableListTile('핸디캡', _userAccount.handicap.toString(), true),
+          _buildEditableListTile('핸디캡', _userAccount.handicap.toString(), true, isNumeric: true),
           _buildEditableListTile('집 주소', _userAccount.address, true),
           _buildEditableListTile(
               '생일',
@@ -137,14 +138,16 @@ class _UserInfoPageState extends ConsumerState<UserInfoPage> {
     );
   }
 
-  Widget _buildEditableListTile(String title, String value, bool editable) {
+  Widget _buildEditableListTile(String title, String value, bool editable, {bool isNumeric = false}) {
     return ListTile(
       title: Text(title),
       subtitle: Text(value),
       trailing: editable ? Icon(Icons.chevron_right) : null,
       onTap: editable
           ? () {
-        if (title == '생일') {
+        if (isNumeric) {
+          _showNumberPicker(context, title, int.parse(value));
+        } else if (title == '생일') {
           _showDatePicker(context, title, _userAccount.dateOfBirth);
         } else {
           _showEditDialog(context, title, value);
@@ -153,6 +156,88 @@ class _UserInfoPageState extends ConsumerState<UserInfoPage> {
           : null,
     );
   }
+
+  // NumberPicker를 사용하는 다이얼로그
+  void _showNumberPicker(BuildContext context, String field, int initialValue) {
+    int selectedValue = initialValue;
+
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true, // 화면에 꽉 차도록 설정
+      builder: (context) {
+        return StatefulBuilder(
+          builder: (BuildContext context, StateSetter setState) {
+            return Container(
+              padding: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom,
+              ),
+              decoration: BoxDecoration(
+                color: Color(0xFFF6F6F6),
+                borderRadius: BorderRadius.vertical(top: Radius.circular(40)), // 상단 모서리 둥글게
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Center(
+                    child: Container(
+                      height: 200, // 높이를 200으로 설정
+                      decoration: BoxDecoration(
+                        color: Color(0xFFF6F6F6), // 내부 배경색 (NumberPicker 영역)
+                        borderRadius: BorderRadius.circular(15), // 모서리를 둥글게 설정
+                      ),
+                      padding: EdgeInsets.symmetric(horizontal: 24), // 좌우 패딩 추가
+                      child: NumberPicker(
+                        value: selectedValue,
+                        minValue: 0,
+                        maxValue: 100,
+                        onChanged: (value) {
+                          setState(() {
+                            selectedValue = value;
+                          });
+                        },
+                        selectedTextStyle: TextStyle(
+                          color: Color(0xFF2DC653), // 숫자 색상 2DC653
+                          fontSize: 27,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textStyle: TextStyle(
+                          color: Colors.black45, // 비선택 숫자 색상
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // 선택된 값을 저장
+                        _updateUserInfo(field: field, value: selectedValue.toString());
+                        Navigator.of(context).pop();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Color(0xFF2DC653), // 버튼 배경 색상 2DC653
+                        minimumSize: Size(MediaQuery.of(context).size.width - 40, 50), // 버튼 너비와 높이를 조정
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15), // 버튼 모서리를 둥글게
+                        ),
+                      ),
+                      child: const Text('저장', style: TextStyle(fontSize: 18)), // 폰트 크기를 20으로 설정
+                    ),
+                  ),
+                  SizedBox(height: 10, child: Container(color: Color(0xFFF6F6F6))), // 버튼 아래에 공간을 추가하여 배경색이 보이도록 함
+                ],
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+
+
+
 
   void _showEditDialog(BuildContext context, String field, String value) {
     TextEditingController controller = TextEditingController(text: value);
