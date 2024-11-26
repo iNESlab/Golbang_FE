060461a8a468f182f092b5f3ba4ab13c163f1d5f
Minjeong Kim <101111603+Mingguriguri@users.noreply.github.com>
2024-11-26 11:39:42 +0900

    refactor: 모임 - 중복되는 주석 코드 삭제, 없어진 코드 다시 되돌리기

--- golbang_jb/lib/pages/group/group_main.dart
+++ golbang_jb/lib/pages/group/group_main.dart
@@ -23,37 +23,6 @@ class _GroupMainPageState extends ConsumerState<GroupMainPage> {
   // Set the number of items per page as a configurable variable
   static const int itemsPerPage = 6;
 
-  // Future<void> _checkAndNavigateToCommunity() async {
-  //   final communityId = Get.arguments?['communityId'];
-  //
-  //   if (communityId != null) {
-  //
-  //     // groupService를 사용해 그룹 정보를 가져옴
-  //     final storage = ref.read(secureStorageProvider);
-  //     final GroupService groupService = GroupService(storage);
-  //     final targetGroupId = Get.arguments?['communityId'];
-  //
-  //     if (targetGroupId != null) {
-  //       List<Group> group = await groupService.getGroupInfo(targetGroupId);
-  //       final communityName = group[0].name;
-  //       final communityImage = group[0].image;
-  //
-  //       // UI가 빌드된 후 CommunityMain 페이지로 이동
-  //       WidgetsBinding.instance.addPostFrameCallback((_) {
-  //         Navigator.push(
-  //           context,
-  //           MaterialPageRoute(
-  //             builder: (context) => CommunityMain(
-  //               communityName: communityName,
-  //               communityImage: communityImage ?? '', // 이미지가 없을 때 대비
-  //             ),
-  //           ),
-  //         );
-  //       });
-  //     }
-  //   }
-  // }
-
   Future<void> _checkAndNavigateToCommunity() async {
     final communityId = Get.arguments?['communityId'];
 
