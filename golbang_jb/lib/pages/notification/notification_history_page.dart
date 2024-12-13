23e589001eec868ef0d5778ccdeb6f9c972f36cb
Kojungbeom <rhwndqja@icloud.com>
2024-12-13 23:35:43 +0900

    Hotfix: 알림 눌렀을 때 모임 or event로 네비게이션 기능 구현

--- golbang_jb/lib/pages/notification/notification_history_page.dart
+++ golbang_jb/lib/pages/notification/notification_history_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get/get.dart';
+import 'package:golbang/pages/event/event_detail.dart';
+import 'package:golbang/pages/home/home_page.dart';
 import 'package:timeago/timeago.dart' as timeago;
 import '../../services/notification_service.dart';
 import '../../repoisitory/secure_storage.dart';
@@ -27,8 +30,16 @@ class NotificationHistoryPageState extends ConsumerState<NotificationHistoryPage
 
     try {
       final data = await notificationService.fetchNotifications();
+      print(data);
       setState(() {
-        notifications = data;
+        notifications = data.map((notification) {
+          // 임시적으로 eventId나 groupId를 추가
+          return {
+            ...notification,
+            'eventId': 36, // 임시 eventId
+            'groupId': 5, // 임시 groupId
+          };
+        }).toList();
         isLoading = false;
       });
     } catch (e) {
@@ -57,6 +68,28 @@ class NotificationHistoryPageState extends ConsumerState<NotificationHistoryPage
       );
     }
   }
+
+  void _navigateToDetailPage(Map<String, dynamic> notification) {
+    final eventId = notification['eventId'];
+    final groupId = notification['groupId'];
+    if (eventId != null) {
+      Get.offAll(() => const HomePage(), arguments: {
+        'initialIndex': 1,
+        'eventId': eventId
+      });
+    } else if (groupId != null) {
+      Get.offAll(() => const HomePage(), arguments: {
+        'initialIndex': 2,
+        'communityId': groupId
+      });
+    } else {
+      // 예외 처리: 이동할 수 없음
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('상세 정보를 확인할 수 없습니다.')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -69,9 +102,9 @@ class NotificationHistoryPageState extends ConsumerState<NotificationHistoryPage
         centerTitle: true,
       ),
       body: isLoading
-          ? Center( // "불러오는 중입니다"를 화면 중앙에 위치
+          ? Center(
         child: Column(
-          mainAxisSize: MainAxisSize.min, // 필요한 만큼만 크기 차지
+          mainAxisSize: MainAxisSize.min,
           children: const [
             CircularProgressIndicator(),
             SizedBox(height: 20),
@@ -83,9 +116,9 @@ class NotificationHistoryPageState extends ConsumerState<NotificationHistoryPage
         ),
       )
           : notifications.isEmpty
-          ? Center( // "알림이 없습니다"를 화면 정중앙에 표시
+          ? Center(
         child: Column(
-          mainAxisSize: MainAxisSize.min, // 필요한 만큼만 크기 차지
+          mainAxisSize: MainAxisSize.min,
           children: const [
             Icon(Icons.notifications_off, size: 50, color: Colors.grey),
             SizedBox(height: 20),
@@ -96,14 +129,23 @@ class NotificationHistoryPageState extends ConsumerState<NotificationHistoryPage
           ],
         ),
       )
-          : Column( // 알림이 있는 경우 상단에 제목 표시
+          : Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Text(
-              '알림',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  '알림',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  '스와이프하여 삭제할 수 있습니다.',
+                  style: const TextStyle(fontSize: 14, color: Colors.grey),
+                ),
+              ],
             ),
           ),
           Expanded(
@@ -132,11 +174,17 @@ class NotificationHistoryPageState extends ConsumerState<NotificationHistoryPage
                       notification['title'],
                       style: const TextStyle(
                           fontSize: 16, fontWeight: FontWeight.bold),
+                      overflow: TextOverflow.ellipsis, // 텍스트 오버플로우 처리
+                      maxLines: 1, // 한 줄로 제한
                     ),
                     subtitle: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(notification['body']),
+                        Text(
+                          notification['body'],
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 2, // 두 줄로 제한
+                        ),
                         const SizedBox(height: 4),
                         Text(
                           relativeTime, // "몇 분 전" 표시
@@ -149,7 +197,7 @@ class NotificationHistoryPageState extends ConsumerState<NotificationHistoryPage
                         ? const Icon(Icons.check_circle, color: Colors.green)
                         : const Icon(Icons.circle, color: Colors.grey),
                     onTap: () {
-                      print("알림 클릭: ${notification['notification_id']}");
+                      _navigateToDetailPage(notification); // 알림 클릭 시 상세 페이지로 이동
                     },
                   ),
                 );
