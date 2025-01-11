49351e31dcf6376d1988b8315b6f467a3db4e1a8
loading1031 <tjdans1031@gmail.com>
2025-01-12 02:26:36 +0900

    refactor: API 호출시 dioClient를 사용하도록 리팩토링

--- golbang_jb/lib/services/club_member_service.dart
+++ golbang_jb/lib/services/club_member_service.dart
@@ -1,40 +1,28 @@
-import 'dart:convert';
+import 'dart:developer';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:http/http.dart' as http;
+import '../global/LoginInterceptor.dart';
 import '../models/profile/member_profile.dart';
 import '../repoisitory/secure_storage.dart';
 
 class ClubMemberService {
   final SecureStorage storage;
+  final dioClient = DioClient();
 
   ClubMemberService(this.storage);
 
   Future<List<ClubMemberProfile>> getClubMemberProfileList({
     required int club_id,
   }) async {
-    // 액세스 토큰 불러오기
-    final accessToken = await storage.readAccessToken();
 
     // API URI 설정
-    var uri = Uri.parse("${dotenv.env['API_HOST']}/api/v1/clubs/$club_id/members/");
-
-    // 요청 헤더 설정
-    Map<String, String> headers = {
-      "Content-type": "application/json",
-      "Authorization": "Bearer $accessToken"
-    };
-
+    var uri = "${dotenv.env['API_HOST']}/api/v1/clubs/$club_id/members/";
     // API 요청
-    var response = await http.get(uri, headers: headers);
-    print("${json.decode(utf8.decode(response.bodyBytes))}");
+    var response = await dioClient.dio.get(uri);;
 
     // 응답 코드가 200(성공)인지 확인
     if (response.statusCode == 200) {
       // JSON 데이터 파싱
-      final jsonData = jsonDecode(utf8.decode(response.bodyBytes));
-      print("json: ${jsonData['data']}");
-
-      return (jsonData['data'] as List)
+      return (response.data['data'] as List)
           .map((json) => ClubMemberProfile.fromJson(json))
           .toList();
 
