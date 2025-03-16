e70835d04bdbb886fbac437fe992ae0b4c4512da
loading1031 <tjdans1031@gmail.com>
2025-03-16 09:49:18 +0900

    Fix: 골프 코스 model - 이벤트 조회시 null에러 발생 - holePars, holeHandicaps가 이전 Course모델에서 사용됐음 - 추가하돼, 빈 리스트로 반환하게 처리

--- golbang_jb/lib/models/responseDTO/CourseResopnseDTO.dart
+++ golbang_jb/lib/models/responseDTO/CourseResopnseDTO.dart
@@ -3,15 +3,26 @@ class CourseResponseDTO {
   final String golfCourseName;
   final int holes;
   final int par;
+  final List<int> holePars;
+  final List<int> holeHandicaps;
 
-  CourseResponseDTO(this.golfCourseId, this.golfCourseName, this.holes, this.par);
+  CourseResponseDTO({
+    required this.golfCourseId,
+    required this.golfCourseName,
+    required this.holes,
+    required this.par,
+    required this.holePars,
+    required this.holeHandicaps,
+  });
 
   factory CourseResponseDTO.fromJson(Map<String, dynamic> json) {
     return CourseResponseDTO(
-      json['golf_course_id'],
-      json['golf_course_name'],
-      json['holes'],
-      json['par'],
+      golfCourseId: json['golf_course_id'],
+      golfCourseName: json['golf_course_name'],
+      holes: json['holes'],
+      par: json['par'],
+      holePars: List<int>.from(json['hole_pars'] ?? []),
+      holeHandicaps: List<int>.from(json['hole_handicaps'] ?? []),
     );
   }
   // 서버로부터 응답받은 것이므로 toJson은 필요없음.
