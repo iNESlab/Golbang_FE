78234db7ec7a1f4cc209670aaa531230e2f89632
loading1031 <tjdans1031@gmail.com>
2025-03-23 02:32:44 +0900

    Refactor: API 제거 Group 및 Member로 통합 - 구성된 필드가 Group에 속하는 변수이고, 앞 페이지에서 이미 Group으로 통합해서 주기 때문에 그대로 가져오도록 변경. - 이미 Club 정보는 앞 페이지에서 받아오기 때문에,API 불러오는 코드 제거 - 이후, widget.club 사용

--- golbang_jb/lib/pages/community/community_main.dart
+++ golbang_jb/lib/pages/community/community_main.dart
@@ -1,71 +1,42 @@
-import 'dart:developer';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../repoisitory/secure_storage.dart';
-import '../../services/club_member_service.dart';
-import 'package:golbang/models/profile/member_profile.dart';
+import '../../models/group.dart';
+import '../../models/member.dart';
 import 'admin_settings_page.dart';
 import 'member_settings_page.dart';
 
-class CommunityMain extends ConsumerStatefulWidget {
-  final int communityID;
-  final String communityName;
-  final String communityImage;
-  final List<String> adminNames;
-  final bool isAdmin;
+class CommunityMain extends StatefulWidget {
+  final Group club;
 
   const CommunityMain({super.key, 
-    required this.communityID,
-    required this.communityName,
-    required this.communityImage,
-    required this.adminNames,
-    required this.isAdmin,
+    required this.club
   });
 
   @override
   _CommunityMainState createState() => _CommunityMainState();
 }
 
-class _CommunityMainState extends ConsumerState<CommunityMain> {
-  List<ClubMemberProfile> members = [];
-  bool isLoading = true;
-  late ClubMemberService _clubMemberService;
+class _CommunityMainState extends State<CommunityMain> {
+  late List<Member> members;
 
   @override
   void initState() {
     super.initState();
-    _clubMemberService = ClubMemberService(ref.read(secureStorageProvider));
-    fetchGroupMembers();
-  }
-
-  void fetchGroupMembers() async {
-    try {
-      final fetchedMembers = await _clubMemberService.getClubMemberProfileList(club_id: widget.communityID);
-      setState(() {
-        members = fetchedMembers;
-        isLoading = false;
-      });
-    } catch (e) {
-      log('Error fetching members: $e');
-      setState(() {
-        isLoading = false;
-      });
-    }
+    members = widget.club.members.where((m)=>m.role != 'admin').toList();
   }
 
   void _onSettingsPressed() {
-    if (widget.isAdmin) {
+    if (widget.club.isAdmin) {
       Navigator.push(
         context,
         MaterialPageRoute(
-          builder: (context) => AdminSettingsPage(clubId: widget.communityID),
+          builder: (context) => AdminSettingsPage(club: widget.club),
         ),
       );
     } else {
       Navigator.push(
         context,
         MaterialPageRoute(
-          builder: (context) => MemberSettingsPage(clubId: widget.communityID),
+          builder: (context) => MemberSettingsPage(clubId: widget.club.id),
         ),
       );
     }
@@ -82,9 +53,9 @@ class _CommunityMainState extends ConsumerState<CommunityMain> {
                 height: 200,
                 decoration: BoxDecoration(
                   image: DecorationImage(
-                    image: widget.communityImage.contains('https') // 문자열 검사
-                        ? NetworkImage(widget.communityImage) // 네트워크 이미지
-                        : AssetImage(widget.communityImage) as ImageProvider, // 로컬 이미지
+                    image: widget.club.image.contains('https') // 문자열 검사
+                        ? NetworkImage(widget.club.image) // 네트워크 이미지
+                        : AssetImage(widget.club.image) as ImageProvider, // 로컬 이미지
                     fit: BoxFit.cover, // 이미지 맞춤 설정
                   ),
                 ),
@@ -113,7 +84,7 @@ class _CommunityMainState extends ConsumerState<CommunityMain> {
                 bottom: 20,
                 left: 10,
                 child: Text(
-                  widget.communityName,
+                  widget.club.name,
                   style: const TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
                 ),
               ),
@@ -121,20 +92,20 @@ class _CommunityMainState extends ConsumerState<CommunityMain> {
           ),
           Padding(
             padding: const EdgeInsets.all(16.0),
-            child: isLoading
-                ? const Center(child: CircularProgressIndicator())
-                : Align(
+            child: Align(
               alignment: Alignment.centerLeft, // 전체 내용을 왼쪽 정렬
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start, // 자식 위젯들을 왼쪽 정렬
                 children: [
                   Text(
-                    '관리자: ${widget.adminNames.join(', ')}', // 여러 관리자 이름 표시
+                    '관리자: ${widget.club.getAdminNames().join(', ')}', // 여러 관리자 이름 표시
                     style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                   ),
                   const SizedBox(height: 8), // 텍스트 간 간격 추가
                   Text(
-                    '멤버: ${members.map((member) => member.name).join(', ')}',
+                    '멤버: ${members.isNotEmpty
+                        ? members.map((member) => member.name).join(', ')
+                        : '새로운 멤버를 초대해주세요'}',
                     style: const TextStyle(fontSize: 16),
                   ),
                 ],
