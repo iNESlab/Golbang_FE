1a8c383d7515ea324d05d0d94f450b9a903423eb
loading1031 <tjdans1031@gmail.com>
2025-03-25 01:20:51 +0900

    Refactor: 커뮤니티에서 뒤로가기시, 속한 모임 재 조회 및 상태 구독 - PopScope의 이해가 낮아 일단 임시 조치로 이렇게 했습니다.

--- golbang_jb/lib/pages/community/community_main.dart
+++ golbang_jb/lib/pages/community/community_main.dart
@@ -1,3 +1,4 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../models/club.dart';
@@ -43,80 +44,100 @@ class _CommunityMainState extends ConsumerState<CommunityMain> {
     if (_club == null) {
       return const Center(child: CircularProgressIndicator());
     }
+    log('뒤로가기11');
+
 
     members = _club!.members.where((m) => m.role != 'admin').toList() ?? [];
 
-    return Scaffold(
-      body: Column(
-        children: [
-          Stack(
-            children: [
-              Container(
-                height: 200,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: _club!.image.contains('https') // 문자열 검사
-                        ? NetworkImage(_club!.image) // 네트워크 이미지
-                        : AssetImage(_club!.image) as ImageProvider, // 로컬 이미지
-                    fit: BoxFit.cover, // 이미지 맞춤 설정
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (didPop) async {
+        log('뒤로가기22');
+        await ref.read(clubStateProvider.notifier).fetchClubs();
+        //TODO: 어째서인지, didPop이 계속 TRUe라 이렇게 위치하게 되었습니다.
+        //PopScope 좀더 공부해서 바꿔야함..
+        if (didPop) {
+          return;
+        }
+        log('뒤로가기33');
+
+        if (context.mounted) {
+          Navigator.of(context).pop();
+        }
+      },
+      child: Scaffold(
+        body: Column(
+          children: [
+            Stack(
+              children: [
+                Container(
+                  height: 200,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: _club!.image.contains('https') // 문자열 검사
+                          ? NetworkImage(_club!.image) // 네트워크 이미지
+                          : AssetImage(_club!.image) as ImageProvider, // 로컬 이미지
+                      fit: BoxFit.cover, // 이미지 맞춤 설정
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                height: 200,
-                color: Colors.black.withOpacity(0.5),
-              ),
-              Positioned(
-                top: 40,
-                left: 10,
-                child: IconButton(
-                  icon: const Icon(Icons.arrow_back, color: Colors.white),
-                  onPressed: () => Navigator.of(context).pop(),
+                Container(
+                  height: 200,
+                  color: Colors.black.withOpacity(0.5),
                 ),
-              ),
-              Positioned(
-                top: 40,
-                right: 10,
-                child: IconButton(
-                  icon: const Icon(Icons.settings, color: Colors.white),
-                  onPressed: _onSettingsPressed,
-                ),
-              ),
-              Positioned(
-                bottom: 20,
-                left: 10,
-                child: Text(
-                  _club!.name,
-                  style: const TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
-                ),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Align(
-              alignment: Alignment.centerLeft, // 전체 내용을 왼쪽 정렬
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start, // 자식 위젯들을 왼쪽 정렬
-                children: [
-                  Text(
-                    '관리자: ${_club!.getAdminNames().join(', ')}', // 여러 관리자 이름 표시
-                    style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                Positioned(
+                  top: 40,
+                  left: 10,
+                  child: IconButton(
+                    icon: const Icon(Icons.arrow_back, color: Colors.white),
+                    onPressed: () => Navigator.of(context).pop(),
                   ),
-                  const SizedBox(height: 8), // 텍스트 간 간격 추가
-                  Text(
-                    '멤버: ${members.isNotEmpty
-                        ? members.map((member) => member.name).join(', ')
-                        : '새로운 멤버를 초대해주세요'}',
-                    style: const TextStyle(fontSize: 16),
+                ),
+                Positioned(
+                  top: 40,
+                  right: 10,
+                  child: IconButton(
+                    icon: const Icon(Icons.settings, color: Colors.white),
+                    onPressed: _onSettingsPressed,
                   ),
-                ],
+                ),
+                Positioned(
+                  bottom: 20,
+                  left: 10,
+                  child: Text(
+                    _club!.name,
+                    style: const TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Align(
+                alignment: Alignment.centerLeft, // 전체 내용을 왼쪽 정렬
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start, // 자식 위젯들을 왼쪽 정렬
+                  children: [
+                    Text(
+                      '관리자: ${_club!.getAdminNames().join(', ')}', // 여러 관리자 이름 표시
+                      style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 8), // 텍스트 간 간격 추가
+                    Text(
+                      '멤버: ${members.isNotEmpty
+                          ? members.map((member) => member.name).join(', ')
+                          : '새로운 멤버를 초대해주세요'}',
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
 
-        ],
+          ],
+        ),
       ),
     );
   }
+
 }
